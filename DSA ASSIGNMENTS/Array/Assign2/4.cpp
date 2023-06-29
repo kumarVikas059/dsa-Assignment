b7b3563faa2e6706ca78c8d9f7a9c1c157c51594
Vikas Kumar <122825125+kumarVikas059@users.noreply.github.com>
2023-06-29 20:43:04 +0530

    Update 4.cpp

--- DSA ASSIGNMENTS/Array/Assign2/4.cpp
+++ DSA ASSIGNMENTS/Array/Assign2/4.cpp
@@ -3,17 +3,44 @@
 #include<unordered_set>
 #include<vector>
 using namespace std;
-int distributeCandies(vector<int>& candyType) {
-        unordered_set<int> set;
-        int n=candyType.size();
-        for(int i=0;i<n;i++){
-            set.insert(candyType[i]);
+bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int count=0;
+        
+        int s=flowerbed.size();
+        if(s==1 && flowerbed[0]==0){
+           count++; 
+           return count;
         }
-        int ans=static_cast<int>(set.size());
-        return ans>n/2?n/2:ans;
+        
+        if(flowerbed[0]==0 && flowerbed[1]==0)
+        {   count++;
+            flowerbed[0]=1;
+        }
+        if(flowerbed[s-1]==0 && flowerbed[s-2]==0){
+            count++;
+            flowerbed[s-1]=1;
+        }
+        
+        for(int i=1;i+1<s;i++){
+            int j=i-1;
+            int k=i+1;
+            if(flowerbed[i]==0){
+                if(flowerbed[j]==0 && flowerbed[k]==0)
+                {
+                        count++;
+                        flowerbed[i]=1;
+                 }
+            }
+        
+        }
+        if(count>=n)
+        return true;
+        else 
+        return false;
     }
 int main(){
-    vector<int> nums={1,1,2,2,3};
-    cout<<distributeCandies(nums)<<endl;
+    int n=2;
+    vector<int> nums={1,0,0,0,1,0,0};
+    cout<<canPlaceFlowers(nums,n)<<endl;
     return 0;
 }
